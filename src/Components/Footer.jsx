3ba37100337fe98d1f624356acb05d2fa52c4817
sarakhi20 <sarakhiammu96@gmail.com>
2024-09-12 11:28:09 +0530

    first commit

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="py-5 bg-dark">
+        <div className="container">
+          <p className="m-0 text-center text-white">
+           chennai{" "}
+           
+            2024
+          </p>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
